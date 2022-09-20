fabe292722c01952d4a36bda48bf7cdab7e9b0a3
MariaDuda <mariabevenuto062@gmail.com>
2022-09-20 16:17:50 -0300

    [FEAT] base of integration done

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -1,3 +1,19 @@
 import app from "../../src/app";
 import supertest from "supertest";
-import { prisma } from "../../src/database";
+import * as scenarioFactory from "../factories/scenarioFactory";
+
+
+beforeEach(async () => {
+    await scenarioFactory.deleteAllData();
+});
+
+const server = supertest(app);
+
+describe("test Route POST /recommendations", () => {
+    it.todo("create a recommendation , send correct format , it should return 201")
+});
+
+
+afterAll(async () => {
+    await scenarioFactory.disconnectPrisma();
+});
