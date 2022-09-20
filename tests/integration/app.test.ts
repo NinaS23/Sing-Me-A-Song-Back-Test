385286a76b22e00727d4752076b188f46054975f
MariaDuda <mariabevenuto062@gmail.com>
2022-09-20 16:23:42 -0300

    [FEAT] implemented tests , without logic

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -13,6 +13,30 @@ describe("test Route POST /recommendations", () => {
     it.todo("create a recommendation , send correct format , it should return 201")
 });
 
+describe("test Route POST /recommendations/:id/upvote", () => {
+    it.todo("")
+});
+
+describe("test Route POST /recommendations/:id/downvote", () => {
+    it.todo("")
+});
+
+describe("test Route GET /recommendations", () => {
+    it.todo("")
+});
+
+describe("test Route GET /recommendations/:id", () => {
+    it.todo("")
+});
+
+describe("test Route GET /recommendations/random", () => {
+    it.todo("")
+});
+
+describe("test Route GET /recommendations/top/:amount", () => {
+    it.todo("")
+});
+
 
 afterAll(async () => {
     await scenarioFactory.disconnectPrisma();
