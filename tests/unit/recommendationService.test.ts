f75f405f448931f693bda3de0b113fb3b0386046
MariaDuda <mariabevenuto062@gmail.com>
2022-09-22 13:53:00 -0300

    [FEAT] test unit

---
+++ tests/unit/recommendationService.test.ts
@@ -0,0 +1,35 @@
+import { jest } from "@jest/globals";
+
+
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+});
+
+describe("creation", () => {
+    it.todo("should insert a recommendation",);
+});
+
+describe("", () => {
+    it.todo("",);
+});
+
+describe("", () => {
+    it.todo(" ",);
+});
+
+describe("", () => {
+    it.todo("",);
+});
+
+describe("", () => {
+    it.todo("",);
+});
+
+describe("", () => {
+    it.todo("",);
+});
+
+describe("", () => {
+    it.todo("",);
+});
